fabf6949051c86d605443e6a40f24e416d61df80
Huijiro <huijirohankei@gmail.com>
2024-12-23 05:24:55 -0300

    fix: redirect from login page to home in case of logged in

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,13 @@
-"use client";
-
 import LoginForm from "@/components/forms/LoginForm";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 // WARN: The second form is need for NextJS server actions
-export default function Login() {
+export default async function Login() {
+  const cookie = await cookies();
+  if (cookie.get("Authorization")) {
+    redirect("/");
+  }
   return (
     <main className="h-screen flex items-center justify-center">
       <LoginForm />
